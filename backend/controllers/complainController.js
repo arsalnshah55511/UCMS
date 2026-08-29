56605551c3ad409ea81721e008c7f46e4b7ca668
Arsalan shah <arsalanshah55511@gmail.com>
2026-08-29 17:50:56 +0500

    added similar complaint to be fetched again

--- backend/controllers/complainController.js
+++ backend/controllers/complainController.js
@@ -3,7 +3,7 @@ const Complaint  = require("../models/Complain")
 const Notification = require("../models/Notification")
 const {analyzeComplaint} = require("../utils/spellCorrector")
 const {generateInsights} = require("../utils/insights")
-const {findSimilarComplaints} = require("../utils/similarity")
+const {findSimilarComplaints} = require("../utils/Similarity")
 const {
      ROLES,
   DEPARTMENT_LIST,
